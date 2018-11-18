5d39f9ba170faa9d5fec8817be59bfffeddc0edc
LvZhihao <zhihao.lv@accenture.com>
2018-11-18 23:26:51 +0800

    can slected date

--- pages/FlightStatus/flightStatusDetail.js
+++ pages/FlightStatus/flightStatusDetail.js
@@ -5,6 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
+    selectedIndex: 2,
+
     dateStrings: [
       'Today (10 Nov)',
       'Wednesday (11 Nov)',
@@ -14,59 +16,72 @@ Page({
     ]
   },
 
+  selectDate: function (e) {
+    var that = this;
+    that.setData({
+      selectedIndex: e.currentTarget.dataset.index
+    })
+    this.toView();
+    console.log('选中的index = ' + this.data.selectedIndex);
+  },
+
+  toView: function (e) {
+    console.log('here');
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
